8d77c0e1d390de2d93b094b1310199bf016a1a9a
SeekingMini <SeekingMini@gmail.com>
2019-12-30 19:49:18 +0800

    Modify the structure of the project.

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
+cd cminus
 yacc -d cminus.y
 lex cminus.l
-cc -o cminus lex.yy.c y.tab.c emitcode.c symtab.c
+cc -o cminus.out lex.yy.c y.tab.c emitcode.c symtab.c
 rm y.tab.c 
 rm y.tab.h
 rm lex.yy.c 
+mv cminus.out ../build
--- out/output.asm
+++ out/output.asm
@@ -1,88 +1,10 @@
 SECTION .data
-                                                
+a times 5 dd 0
+                                 
 ReturnMsg: db "Return Value:%i", 10, 0
 
 SECTION .text
 extern printf
-global factorial
-factorial:
-push ebp
-mov ebp, esp
-push ebx
-push esi
-push edi
-sub esp, 16
-
-mov eax, 4
-mov [ebp-12], eax
-mov eax, 44
-mov [ebp-16], eax
-mov eax, [ebp+8]
-mov ebx, 1
-cmp eax, ebx
-je Equ0
-mov eax, 0
-jmp EndEqu0
-Equ0:
-mov eax, 1
-EndEqu0:
-cmp eax, 1
-jne EndIf1
-mov eax, 1
-
-;Print return value
-push eax
-push ReturnMsg
-call printf
-add esp, 4
-pop eax
-pop edi
-pop esi
-pop ebx
-mov esp,ebp
-pop ebp
-ret
-
-jmp EndIfElse1
-EndIf1:
-mov eax, [ebp+8]
-mov ebx, [ebp+8]
-mov ecx, 1
-sub ebx, ecx
-
-;Before call
-;Save required resgisters
-push eax
-;Push params
-push ebx
-call factorial
-
-;After call
-;Store return value
-mov ebx, eax
-
-;Clear stack of params
-add esp, 4
-
-;Restore used resgisters
-pop eax
-
-imul eax, ebx
-
-;Print return value
-push eax
-push ReturnMsg
-call printf
-add esp, 4
-pop eax
-pop edi
-pop esi
-pop ebx
-mov esp,ebp
-pop ebp
-ret
-
-EndIfElse1:
 global main
 main:
 push ebp
@@ -91,4 +13,72 @@ push ebx
 push esi
 push edi
 sub esp, 16
+
+mov eax, 0
+mov [ebp-12], eax
+While0:
+mov eax, [ebp-12]
+mov ebx, 5
+cmp eax, ebx
+jl Lt1
+mov eax, 0
+jmp EndLt1
+Lt1:
+mov eax, 1
+EndLt1:
+cmp eax, 1
+jne EndWhile0
+mov eax, [ebp-12]
+mov ebx, [ebp-12]
+mov ecx, a
+mov [ecx+4*eax], ebx
+mov ebx, [ebp-12]
+mov ecx, 1
+add ebx, ecx
+mov [ebp-12], ebx
+jmp While0
+EndWhile0:
+mov ebx, 0
+mov [ebp-12], ebx
+mov ebx, 0
+mov [ebp-16], ebx
+While2:
+mov ebx, [ebp-12]
+mov ecx, 5
+cmp ebx, ecx
+jl Lt3
+mov ebx, 0
+jmp EndLt3
+Lt3:
+mov ebx, 1
+EndLt3:
+cmp ebx, 1
+jne EndWhile2
+mov ebx, [ebp-16]
+mov ecx, [ebp-12]
+mov esi, a
+mov edx, [esi+4*ecx]
+add ebx, edx
+mov [ebp-16], ebx
+mov ecx, [ebp-12]
+mov edx, 1
+add ecx, edx
+mov [ebp-12], ecx
+jmp While2
+EndWhile2:
+mov ecx, 0
+mov eax, ecx
+
+;Print return value
+push eax
+push ReturnMsg
+call printf
+add esp, 4
+pop eax
+pop edi
+pop esi
+pop ebx
+mov esp,ebp
+pop ebp
+ret
 
